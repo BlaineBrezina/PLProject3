8b3505e7bfb13cf83553e4170e1888d2251f98c0
Kayleigh Marie Escudero <cherrydrpepper18@gmail.com>
2016-03-07 18:50:04 -0600

    preload working & changing images working

--- brezina/image_gallery/image_gallery.js
+++ brezina/image_gallery/image_gallery.js
@@ -5,13 +5,13 @@ $(document).ready( function () {
 
 
     //preload the images
-    //$.fn.preload = function() {
-    //    this.each(function(){
-    //        alert("the image has been loaded");
-    //        $('<img/>')[0].src = $(this).attr("href");
+    $.fn.preload = function() {
+        this.each(function(){
+           // alert("the image has been loaded");
+            $('<img/>')[0].src = $(this).attr("href");
 
-    //    });
-    //};
+        });
+    };
 
 // Usage:
 
